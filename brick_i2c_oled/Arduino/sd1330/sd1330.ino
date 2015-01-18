c773ef937efc1d4cd51a832434eae70f3a03e6a1
Akira-Sasaki <gclue.akira@gmail.com>
2015-01-18 23:44:47 +0900

    add oled i2c sample

---
+++ brick_i2c_oled/Arduino/sd1330/sd1330.ino
@@ -0,0 +1,22 @@
+#include "U8glib.h"
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);	
+
+void draw() {
+
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr( 0, 20, "FaBo");
+}
+
+void setup() {
+}
+
+void loop() {
+
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+    delay(1000);
+}
+
