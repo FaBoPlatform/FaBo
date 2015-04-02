7087a9ae2b6bd8673b2a514e9f0001a9a7bd269a
Akira-Sasaki <gclue.akira@gmail.com>
2015-04-02 11:54:22 +0900

    Modified PCB

--- brick_analog_temp/Arduino/TempSample1/TempSample1.ino
+++ brick_analog_temp/Arduino/TempSample1/TempSample1.ino
@@ -1,3 +1,4 @@
+int temp_pin = A0;
 int temp_value = 0;
 
 void setup() {                
@@ -5,7 +6,7 @@ void setup() {
 }
 
 void loop() {
-  temp_value = analogRead(A0) ;  
+  temp_value = analogRead(temp_pin) ;  
   temp_value  = map(temp_value,0,1023,0,5000) ;    
   temp_value = map(temp_value,300,1600,-30,100) ; 
   Serial.println(temp_value) ; 
