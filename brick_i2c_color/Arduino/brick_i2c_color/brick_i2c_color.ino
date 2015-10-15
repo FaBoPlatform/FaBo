a2a926bc7ffe4ae363d5c24a74227a602f306f9f
Shimakage <shimakage@gclue.jp>
2015-10-15 15:42:06 +0900

    modified Arduino Sample

--- brick_i2c_color/Arduino/brick_i2c_color/brick_i2c_color.ino
+++ brick_i2c_color/Arduino/brick_i2c_color/brick_i2c_color.ino
@@ -3,148 +3,88 @@
 //
 // brick_i2c_color
 //
-
 #include <Wire.h>
 
-#define DEVICE_ADDR (0x2A) // スレーブデバイスのアドレス
+#define DEVICE_ADDR 0x2A // スレーブデバイスのアドレス
+#define CTLREG 0x00      
+
+uint16_t red = 0;
+uint16_t green = 0;
+uint16_t blue = 0;
+uint16_t infrared = 0;
 
 void setup()
 {
-  Serial.begin(9600); // シリアルの開始デバック用
-  Wire.begin();       // I2Cの開始
-  
-  Serial.println("Checking I2C device...");
-  byte who_am_i = 0x00;
-  readI2c(0x2A, 1, &who_am_i);
-  Serial.println(who_am_i);
-  if(who_am_i == 0x03){
-    Serial.println("I am MMA8451");
-  }else{
-    Serial.println("Not detected");
-  }
-  
-  Serial.println("init");
-
-  
+  Serial.begin(9600); //シリアル開始 表示用
+  Wire.begin();       //I2C開始
 }
 
 void loop()
-{ 
-  /*
-  int length = 6;
-  byte axis_buff[6];
-  readI2c(0x01, length, axis_buff); //レジスターアドレス 0x32から6バイト読む
-  int x = axis_buff[0] << 2 | axis_buff[1] >> 6 & 0x3;
-  int y = axis_buff[2] << 2 | axis_buff[3] >> 6 & 0x3;
-  int z = axis_buff[4] << 2 | axis_buff[5] >> 6 & 0x3;
-  */
-  /*
-  byte axis_buff[3];
-  readI2c(0x01, 3, axis_buff);
-  int x = axis_buff[0] << 2;
-  int y = axis_buff[1] << 2;
-  int z = axis_buff[2] << 2;
-  */
-  
-  /*
-  byte axis_buff[6];
-  uint8_t length = 6;
-  readI2c(0x01, length, axis_buff); //レジスターアドレス 0x32から6バイト読む
-  
-  int x = (int16_t)((axis_buff[0] << 8) + axis_buff[1]);   
-  int y = (int16_t)((axis_buff[2] << 8) + axis_buff[3]);   
-  int z = (int16_t)((axis_buff[4] << 8) + axis_buff[5]);   
-  
-  
-  int x1 = axis_buff[0] & 0x03;   
-  int x2 = axis_buff[1] >> 2;   
-  int y1 = axis_buff[2] & 0x03;    
-  int y2 = axis_buff[3] >> 2;   
-  int z1 = axis_buff[4] & 0x03;   
-  int z2 = axis_buff[5] >> 2;   
- 
-   
-  Serial.print("x1: ");
-  Serial.print( x1 );
-   Serial.print(" x2: ");
-  Serial.print( x2 );
-  Serial.print(" y1: ");
-  Serial.print( y1 );
-   Serial.print(" y2: ");
-  Serial.print( y2 );
-  Serial.print(" z1: ");
-  Serial.print( z1 );
-  Serial.print(" z2: ");
-  Serial.println( z2 );
-  
-  delay(1000);
-  */
-  
-}
-
-// Standby mode
-void standbyMode()
 {
-   byte n = 0x00;
-   byte activeMask = 0x01;
-  
-  readI2c(0x2a, 1, &n);
-  Serial.println( n );
-  Serial.println( n & ~activeMask );
-  writeI2c(0x2a, n & ~activeMask);
+  //色の取得
+  getColor();
+
+  //色の出力
+  Serial.print("R:[");
+  Serial.print(red);
+  Serial.print("] G:[");
+  Serial.print(green);
+  Serial.print("] B:[");
+  Serial.print(blue);
+  Serial.print("] IR:[");
+  Serial.print(infrared);
+  Serial.println("]");
+
+  delay(500);
 }
 
-// Active Mode
-void activeMode()
+void getColor()
 {
-  byte n = 0x00;
-  byte activeMask = 0x01;
+  uint16_t color= 0;
+
+  // 待機解除
+  Wire.beginTransmission(DEVICE_ADDR);
+  Wire.write(CTLREG);
+  Wire.write(0x83); // ADC reset wakeup
+  Wire.endTransmission(false);
+
+  Wire.beginTransmission(DEVICE_ADDR);
+  Wire.write(CTLREG);
+  Wire.write(0x03);
+  Wire.endTransmission(true);
+
+  delay(500);
+
+  Wire.beginTransmission(DEVICE_ADDR);
+  Wire.write(0x03);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(0x2A,8,true);
  
-  readI2c(0x2a, 1, &n);
-  Serial.println( n );
-  Serial.println( n | activeMask );
-  writeI2c(0x2a, n | activeMask);
-}
+  //色データの取得
 
-void hiresolMode()
-{
-  byte n;
-  byte fastModeMask = 0x02;
- 
-  readI2c(0x2b, 1, &n);
-  writeI2c(0x2b,  n | fastModeMask);
-}
+  // 赤
+  color = Wire.read() << 8;
+  color |= Wire.read();
+  red = color;
 
-// I2Cへの書き込み
-void writeI2c(byte register_addr, byte value) {
-  Wire.beginTransmission(DEVICE_ADDR);  
-  Wire.write(register_addr);         
-  Wire.write(value);                 
-  Wire.endTransmission();        
-}
+  // 緑
+  color =0;
+  color = Wire.read() << 8;
+  color |= Wire.read();
+  green = color;
 
-// I2Cへの読み込み
-void readI2c(byte register_addr, int num, byte *buf) {
-  Wire.beginTransmission(DEVICE_ADDR); 
-  Wire.write(register_addr);           
-  Wire.endTransmission();         
+  // 青
+  color =0;
+  color = Wire.read() << 8;
+  color |= Wire.read();
+  blue = color;
 
-  Wire.beginTransmission(DEVICE_ADDR); 
-  Wire.requestFrom(DEVICE_ADDR, num);  
+  // 赤外
+  color =0;
+  color = Wire.read() << 8;
+  color |= Wire.read();
+  infrared = color;
 
-  int i = 0;
-  while (Wire.available())
-  {
-    byte n = 0x00;
-    n = Wire.read();
-    *(buf + i) = n;
-    //byte buf1 = Wire.read();
-    //int x1 = (int)buf1;
-   
-    Serial.print("Wire.read();");
-    Serial.println(n);
-    
-    i++;   
-  }
-  Wire.endTransmission();         
+  return;
 }
