513fc26060f884ef15265817479987fc0eeea593
Shimakage <shimakage@gclue.jp>
2015-11-13 15:10:09 +0900

    Modified Arduino Sample Limitswitch

--- 107_limitswitch/src/Arduino/limitswitch/limitswitch.ino
+++ 107_limitswitch/src/Arduino/limitswitch/limitswitch.ino
@@ -3,33 +3,31 @@
 //
 // brick_analog_limitswitch
 //
-int ledpin = 5;
-volatile int move_value = 1;
 
-int value = 0;
+#define buttonPin A0 // リミットスイッチピン
+#define ledPin A1    // LEDピン
+
+// リミットスイッチの状況取得用
+int buttonState = 0;
 
 void setup() {
-  pinMode(ledpin, OUTPUT);
-  Serial.begin(9600);
-  attachInterrupt(0, change, FALLING);
-
+  // リミットスイッチピンを入力用に設定
+  pinMode(buttonPin, INPUT); 
+  // LEDピンを出力用に設定
+  pinMode(ledPin, OUTPUT);         
 }
 
-void loop() {
-  value += move_value;
+void loop(){
+  // リミットスイッチの押下状況を取得
+  buttonState = digitalRead(buttonPin);
 
-  if (value > 255) {
-    value = 255;
-  } else if(value < 0) {
-    value = 0;
+  // リミットスイッチ判定
+  if (buttonState == LOW) {        
+    // LED点灯
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+    // LED消灯
+    digitalWrite(ledPin, LOW); 
   }
-  
-  Serial.println(value);
-  analogWrite(ledpin, value);
-
-  delay(100);
-}
-
-void change() {
-  move_value *= -1;
 }
