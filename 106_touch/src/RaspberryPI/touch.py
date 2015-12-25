8163eabbeeb9dc19c9fb2e93e4fe7bee14baaed4
Hideki Yamauchi <hideki.yamauchi@gmail.com>
2015-12-25 17:28:27 +0900

    change raspberrypi sample

--- 106_touch/src/RaspberryPI/touch.py
+++ 106_touch/src/RaspberryPI/touch.py
@@ -1,23 +1,56 @@
+#!/usr/bin/env python
 # coding: utf-8
+
 #
 # FaBo Brick Sample
 #
-# brick_analog_touch
+# #106 Touch Brick
 #
+# A0コネクタにTouchを接続して、GPIO4コネクタに接続したLED Brickの明るさ調節に使用しています。
+
 
 import RPi.GPIO as GPIO
+import spidev
+import time
+import sys
 
+# A0コネクタにTouchを接続
+TOUCHPIN = 0
+
+# GPIO4コネクタにLEDを接続
 LEDPIN = 4
-TOUCHPIN = 7
 
+# GPIOポートを設定
 GPIO.setwarnings(False)
 GPIO.setmode( GPIO.BCM )
 GPIO.setup( LEDPIN, GPIO.OUT )
-GPIO.setup( TOUCHPIN, GPIO.IN )
 
-while True:
-     #圧力がかかっている間、LED点灯
-    if( GPIO.input( TOUCHPIN ) ):
-        GPIO.output( LEDPIN, False )
-    else:
-        GPIO.output( LEDPIN, True )
+# PWM/100Hzに設定
+LED = GPIO.PWM(LEDPIN, 100)
+
+# 初期化
+LED.start(0)
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def readadc(channel):
+	adc = spi.xfer2([1,(8+channel)<<4,0])
+	data = ((adc[1]&3) << 8) + adc[2]
+	return data
+
+def arduino_map(x, in_min, in_max, out_min, out_max):
+	return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+if __name__ == '__main__':
+	try:
+		while True:
+			data = readadc(TOUCHPIN)
+			print("adc : {:8} ".format(data))
+			value = arduino_map(data, 0, 1023, 100, 0)
+			LED.ChangeDutyCycle(value)
+			time.sleep( 0.01 )
+	except KeyboardInterrupt:
+		LED.stop()
+		GPIO.cleanup()
+		spi.close()
+		sys.exit(0)
